17d7c0599c997170d659754f5cf37a5589292235
Tymoteusz <vubes44@gmail.com>
2026-06-27 12:18:35 +0200

    added currentProduct argument to saveConversation function

--- backend/src/services/conversations.ts
+++ backend/src/services/conversations.ts
@@ -5,6 +5,7 @@ export async function saveConversation(
   email: string,
   userMessage: string,
   aiResponse: string,
+  currentProduct?: string,
 ) {
   await db
     .collection("conversations")
