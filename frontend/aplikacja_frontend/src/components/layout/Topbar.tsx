24d450b92bd2c0ba8a9f5cab39d4b6440c3c33f7
Tymoteusz <vubes44@gmail.com>
2026-07-07 07:49:27 +0200

    added Topbar.tsx component

---
+++ frontend/aplikacja_frontend/src/components/layout/Topbar.tsx
@@ -0,0 +1,31 @@
+export default function Topbar() {
+  return (
+    <header className="h-20 border-b border-gray-800 bg-[#111827] flex items-center justify-between px-8">
+
+      <div>
+
+        <h2 className="text-2xl font-bold">
+          Dashboard
+        </h2>
+
+        <p className="text-gray-400 text-sm">
+          AI DJI Store Management
+        </p>
+
+      </div>
+
+      <div className="flex items-center gap-4">
+
+        <button className="w-10 h-10 rounded-xl bg-gray-800 hover:bg-gray-700">
+          🔔
+        </button>
+
+        <button className="w-10 h-10 rounded-full bg-blue-600">
+          A
+        </button>
+
+      </div>
+
+    </header>
+  );
+}
