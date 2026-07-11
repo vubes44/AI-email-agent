ebd8c07737d5614fef88f988ce08d6baacdce61b
Tymoteusz <vubes44@gmail.com>
2026-07-11 13:11:57 +0200

    connected orders endpoint to index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import authRoutes from "./routes/auth.js";
 import productsRoutes from "./routes/products.js";
 import { startEmailWatcher } from "./services/emailWatcherRunner.js";
+import ordersRoutes from "./routes/orders.js";
 
 const app = express();
 
 app.use("/auth", authRoutes);
 app.use("/products", productsRoutes);
+app.use("/orders", ordersRoutes);
 
 app.get("/", (req, res) => {
   res.send("AI Email Agent działa 🚀");
