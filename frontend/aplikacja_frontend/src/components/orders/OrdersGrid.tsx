725262bfc642adf05aa310c104588a38bc794fc9
Tymoteusz <vubes44@gmail.com>
2026-07-23 15:16:50 +0200

    modified OrdersGrid.tsx

--- frontend/aplikacja_frontend/src/components/orders/OrdersGrid.tsx
+++ frontend/aplikacja_frontend/src/components/orders/OrdersGrid.tsx
@@ -2,39 +2,42 @@ import OrderCard from "./OrderCard";
 
 interface Order {
   id: string;
-  customerName: string;
-  customerEmail: string;
-  productName: string;
+  customer_name: string | null;
+  customer_email: string;
+  threadId: string;
+  product_name: string;
   variant: string;
   quantity: number;
   price: number;
   status: string;
+  created_at: {
+    _seconds: number;
+    _nanoseconds: number;
+  };
 }
 
 interface OrdersGridProps {
   orders: Order[];
 }
 
-export default function OrdersGrid({
-  orders,
-}: OrdersGridProps) {
+export default function OrdersGrid({ orders }: OrdersGridProps) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-
+    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
       {orders.map((order) => (
         <OrderCard
           key={order.id}
-          orderId={order.id}
-          customerName={order.customerName}
-          customerEmail={order.customerEmail}
-          productName={order.productName}
+          id={order.id}
+          customer_name={order.customer_name}
+          customer_email={order.customer_email}
+          threadId={order.threadId}
+          product_name={order.product_name}
           variant={order.variant}
           quantity={order.quantity}
           price={order.price}
           status={order.status}
+          created_at={order.created_at}
         />
       ))}
-
     </div>
   );
 }
