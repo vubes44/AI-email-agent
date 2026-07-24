b5927355e673ecd821895651b20b9e7f9190ec4f
Tymoteusz <vubes44@gmail.com>
2026-07-24 13:56:06 +0200

    Corrected ProductCardProps interface in ProductCard.tsx

--- frontend/aplikacja_frontend/src/components/products/ProductCard.tsx
+++ frontend/aplikacja_frontend/src/components/products/ProductCard.tsx
@@ -1,10 +1,30 @@
 interface ProductCardProps {
+  id: string;
   name: string;
   variant: string;
   price: number;
   currency: string;
   quantity: number;
   active: boolean;
+
+  description: string;
+  category: string;
+
+  weight: number;
+  flight_time: number;
+  transmission_range: number;
+
+  cmos: string;
+  photo_resolution: string;
+  video_resolution: string;
+
+  max_speed: string;
+  obstacle_sensing: string;
+
+  intelligent_flight_features: string[];
+
+  created_at: any;
+  updated_at: any;
 }
 
 export default function ProductCard({
